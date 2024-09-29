cbd1cc8b7829b6fb6d94f964ec2062134d4028c3
Vitor Mouro <vitor.mouro@outlook.com>
2024-09-29 17:56:24 -0300

    Include bicycle task code and assets

---
+++ mjpc/tasks/bicycle/bicycle.cc
@@ -0,0 +1,39 @@
+// Copyright 2022 DeepMind Technologies Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mjpc/tasks/bicycle/bicycle.h"
+
+#include <cmath>
+#include <string>
+
+#include <mujoco/mujoco.h>
+#include "mjpc/task.h"
+#include "mjpc/utilities.h"
+
+namespace mjpc {
+std::string Bicycle::XmlPath() const {
+  return GetModelPath("bicycle/task.xml");
+}
+std::string Bicycle::Name() const { return "Bicycle"; }
+
+void Bicycle::ResidualFn::Residual(const mjModel* model, const mjData* data,
+                                    double* residual) const {
+
+  residual[0] = std::cos(data->qpos[1]) - 1;
+  residual[1] = data->qpos[0] - parameters_[0];
+  residual[2] = data->qvel[1];
+  residual[3] = data->ctrl[0];
+}
+
+}  // namespace mjpc
---
+++ mjpc/tasks/bicycle/bicycle.h
@@ -0,0 +1,36 @@
+#ifndef MJPC_TASKS_BICYCLE_BICYCLE_H_
+#define MJPC_TASKS_BICYCLE_BICYCLE_H_
+
+#include <string>
+
+#include <mujoco/mujoco.h>
+#include "mjpc/task.h"
+
+namespace mjpc {
+class Bicycle : public Task {
+ public:
+  std::string Name() const override;
+  std::string XmlPath() const override;
+
+  class ResidualFn : public BaseResidualFn {
+   public:
+    explicit ResidualFn(const Bicycle* task) : BaseResidualFn(task) {}
+
+    void Residual(const mjModel* model, const mjData* data,
+                  double* residual) const override;
+  };
+
+  Bicycle() : residual_(this) {}
+
+ protected:
+  std::unique_ptr<mjpc::ResidualFn> ResidualLocked() const override {
+    return std::make_unique<ResidualFn>(this);
+  }
+  ResidualFn* InternalResidual() override { return &residual_; }
+
+ private:
+  ResidualFn residual_;
+};
+}  // namespace mjpc
+
+#endif  // MJPC_TASKS_BICYCLE_BICYCLE_H_
