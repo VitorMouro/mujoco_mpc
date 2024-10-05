86ff0ac9c483022f1f66e5fa9bbe93e224e07287
VitorMouro <vitor.mouro@outlook.com>
2024-10-05 14:49:17 -0300

    Refactor bicycle and humanoid models; update task settings
    Significantly modified `ResidualFn::Residual` in `bicycle.cc`:
    - Removed velocity and heading target calculations.
    - Added new target velocity and velocity error calculations.
    - Updated residual calculation to include velocity error norm.
    - Added control data copying and sensor dimension checks.
    
    Updated `bicycle.xml`:
    - Removed timestep option.
    - Added new mesh files and geometries for handlebars.
    - Adjusted `seat_site` position.
    - Commented out `<tendon>` and `<actuator>` sections.
    - Modified `pedal` motor gear and `chain_drive` joint's `polycoef`.
    - Added new contact exclusions for the crank.
    
    Adjusted `humanoid.xml`:
    - Updated positions of `right_foot_site` and `left_foot_site`.
    
    Updated `task.xml`:
    - Added timestep option.
    - Modified `numeric` elements under `<custom>`.
    - Updated `Velocity` sensor and added `Control` sensor.
    - Added new contact exclusions for the crank.
    - Changed `connect` to `weld` elements with `solimp` values.
    - Updated `key` element's `qpos` values.

--- mjpc/tasks/bicycle/bicycle.cc
+++ mjpc/tasks/bicycle/bicycle.cc
@@ -30,26 +30,43 @@ std::string Bicycle::Name() const { return "Bicycle"; }
 void Bicycle::ResidualFn::Residual(const mjModel* model, const mjData* data,
                                     double* residual) const {
 
-  // Velocity target
-  double x_vel = SensorByName(model, data, "frame_subtreelinvel")[0];
-  double y_vel = SensorByName(model, data, "frame_subtreelinvel")[1];
-  double resultant_vel = std::sqrt(x_vel * x_vel + y_vel * y_vel);
-  residual[0] = resultant_vel - parameters_[0];
+  int counter = 0;
+
+  // Target speed for the goal heading
+  double speed_goal = parameters_[0];
+  double heading_goal = parameters_[2]; // In radians [-pi, pi]
+  double target_velocity[3] = { speed_goal * cos(heading_goal),
+								speed_goal * sin(heading_goal), 0 };
+  double* currect_velocity = SensorByName(model, data, "frame_subtreelinvel");
+  double velocity_error[3] = { target_velocity[0] - currect_velocity[0],
+							   target_velocity[1] - currect_velocity[1],
+							   target_velocity[2] - currect_velocity[2] };
+  double velocity_error_norm = sqrt(velocity_error[0] * velocity_error[0] +
+	  velocity_error[1] * velocity_error[1] +
+	  velocity_error[2] * velocity_error[2]);
+  residual[counter++] = velocity_error_norm;
 
   // Height target
   double height = SensorByName(model, data, "trace0")[2];
-  residual[1] = height - parameters_[1];
+  residual[counter++] = height - parameters_[1];
 
-  // Heading target
-  double heading_x = SensorByName(model, data, "frame_xaxis")[0];
-  double heading_y = SensorByName(model, data, "frame_xaxis")[1];
-  double heading = std::atan2(heading_y, heading_x);
-  residual[2] = heading - parameters_[2];
-
-  /*residual[0] = std::cos(data->qpos[1]) - 1;
-  residual[1] = data->qpos[0] - parameters_[0];
-  residual[2] = data->qvel[1];
-  residual[3] = data->ctrl[0];*/
+  // ----- action ----- //
+  mjtNum* start = data->ctrl + model->nu - 22;
+  mju_copy(&residual[counter], start, 21);
+  counter += 21;
+  
+  int user_sensor_dim = 0;
+  for (int i = 0; i < model->nsensor; i++) {
+    if (model->sensor_type[i] == mjSENS_USER) {
+      user_sensor_dim += model->sensor_dim[i];
+    }
+  }
+  if (user_sensor_dim != counter) {
+    mju_error_i(
+        "mismatch between total user-sensor dimension "
+        "and actual length of residual %d",
+        counter);
+  }
 }
 
 }  // namespace mjpc
